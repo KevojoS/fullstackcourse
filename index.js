f6d13f7c98ed2e32fa69e5ac1e85276faf03c84e
Kevin S <kaunghtetsan6574@gmail.com>
2025-09-13 22:28:58 -0400

    buildui

--- index.js
+++ index.js
@@ -6,28 +6,57 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-let phonebook = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password, name, and number as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+
+const url = `mongodb+srv://Kevojo:${password}@cluster0.qsr1liu.mongodb.net/phoneBookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Contact = mongoose.model('Contact', contactSchema)
+
+if (process.argv.length == 3) {
+    Contact.find({}).then(result => {
+        result.forEach(note => {
+            console.log(note)
+        })
+        mongoose.connection.close()
+    })
+}
+else {
+    if (process.argv.length < 5) {
+        console.log('give password, name, and number as argument')
+        process.exit(1)
     }
-]
+
+    const name = process.argv[3]
+    const number = process.argv[4]
+
+    const contact = new Contact({
+        name: name,
+        number: number,
+    })
+
+    contact.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+
+}
 
 const generateId = () => {
   const maxid = phonebook.length > 0 ? Math.max(...phonebook.map(number => number.id)) : 0
@@ -44,7 +73,7 @@ app.get('/', (request,response) =>{
     response.send('<h1>Hello World!</h1>')
 })
 app.get('/api/phonebook', (request,response) => {
-    response.json(phonebook)
+  Contact.find({}).then(notes => response.json(notes))
 })
 app.get('/api/phonebook/:id', (request,response)=>{
     const id = request.params.id
